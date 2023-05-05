11ea2ae1a03f4ff64cf449a8bc41cfd17b55213a
Cesar Herrera <Cesar.Herrera@tranzact.net>
2023-05-04 20:00:01 -0500

    Add dbcontext

---
+++ ApiShoppingCart/DbContexts/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using ApiShoppingCart.Models;
+using Microsoft.EntityFrameworkCore;
+namespace ApiShoppingCart.DbContexts
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext() {}
+        public AppDBContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Order> orders { get; set; }
+        public DbSet<OrderItem> orderItems { get; set; }
+    }
+}
