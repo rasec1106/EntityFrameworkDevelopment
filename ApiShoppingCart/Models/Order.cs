f1a12bedc12f9f377a976e845e664b2bfcf4f3a1
Cesar Herrera <Cesar.Herrera@tranzact.net>
2023-05-04 19:44:55 -0500

    Add properties to Order class

--- ApiShoppingCart/Models/Order.cs
+++ ApiShoppingCart/Models/Order.cs
@@ -3,5 +3,11 @@
     public class Order
     {
         public int orderId { get; set; }
+        public int customerId { get; set; }
+        public string? customerName { get; set; }
+        public string? phone { get; set; }
+        public string? email { get; set; }
+        public decimal amount { get; set;}
+
     }
 }
