39d7f608a75e82516adc2ccd8d56f18bca858ffb
Cesar Herrera <Cesar.Herrera@tranzact.net>
2023-05-18 20:47:55 -0500

    Create custom middleware

--- ApiCustomer/Exceptions/GlobalExceptionHandler.cs
+++ ApiCustomer/Exceptions/GlobalExceptionHandler.cs
@@ -1,4 +1,10 @@
-﻿namespace ApiCustomer.Exceptions
+﻿using System.Net;
+using System.Text.Json;
+// These three lines are to distinguish them from EntityFramework classes that have the same name
+using BadRequestException = ApiCustomer.Exceptions.BadRequestException;
+using KeyNotFoundException = ApiCustomer.Exceptions.KeyNotFoundException;
+using NotFoundException = ApiCustomer.Exceptions.NotFoundException;
+namespace ApiCustomer.Exceptions
 {
     // This will be a middleware
     public class GlobalExceptionHandler
@@ -24,6 +30,42 @@
         private static Task HandleExceptionAsync(HttpContext context, Exception ex)
         {
             // ErrorResponseDto structure creation in line
+            HttpStatusCode statusCode;
+            DateTime timeStamp = DateTime.Now;
+            string message;
+            string? stackTrace;
+
+            var exType = ex.GetType();
+            if (exType == typeof(BadRequestException))
+            {
+                statusCode = HttpStatusCode.BadRequest;
+            }
+            else if (exType == typeof(KeyNotFoundException))
+            {
+                statusCode = HttpStatusCode.Unauthorized;
+            }
+            else if (exType == typeof(NotFoundException))
+            {
+                statusCode= HttpStatusCode.NotFound;
+            }
+            else
+            {
+                statusCode = HttpStatusCode.InternalServerError;
+            }
+            message= ex.Message;
+            stackTrace= ex.StackTrace;
+
+            // Creating inline object
+            var ErrorResponseDto = JsonSerializer.Serialize(
+                // serializing a new object    
+                new
+                    {
+                        statusCode, timeStamp, message, stackTrace
+                    }
+                );
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int) statusCode;
+            return context.Response.WriteAsync(ErrorResponseDto);
         }
     }
 }
--- ApiCustomer/Program.cs
+++ ApiCustomer/Program.cs
@@ -31,6 +31,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+/******************************************/
+
+/******************************************/
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
