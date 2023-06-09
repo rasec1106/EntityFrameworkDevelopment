d45dee557ca7c23c3caa3f9c6855e1c9c1e6b725
Cesar Herrera <Cesar.Herrera@tranzact.net>
2023-06-08 20:33:40 -0500

    Call the microservice

--- WebApp/src/app/services/category.service.ts
+++ WebApp/src/app/services/category.service.ts
@@ -11,8 +11,11 @@ export class CategoryService {
   // dependency injection for HttpClient
   constructor(private httpClient:HttpClient) { }
 
+  // The url of my backend endpoint
+  url = "https://localhost:7046/api/category"
+
   // This will be the publisher
   getCategories():Observable<Category[]>{
-
+    return this.httpClient.get<Category[]>(this.url)
   }
 }
