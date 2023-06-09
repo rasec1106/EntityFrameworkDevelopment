d84c26fb43a59f7cf4cb47e8451a38005cd4875a
Cesar Herrera <Cesar.Herrera@tranzact.net>
2023-06-08 20:29:53 -0500

    Create the publisher observable function

--- WebApp/src/app/services/category.service.ts
+++ WebApp/src/app/services/category.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '../model/category';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +10,9 @@ export class CategoryService {
 
   // dependency injection for HttpClient
   constructor(private httpClient:HttpClient) { }
+
+  // This will be the publisher
+  getCategories():Observable<Category[]>{
+
+  }
 }
