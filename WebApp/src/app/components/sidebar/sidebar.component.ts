b4071070352c90c7dcd171c8acb2694fab97bd52
Cesar Herrera <Cesar.Herrera@tranzact.net>
2023-06-08 20:49:50 -0500

    Subscribing to observable

--- WebApp/src/app/components/sidebar/sidebar.component.ts
+++ WebApp/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Category } from 'src/app/model/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-  categories = ["Option 1", "Option 2", "Option 3"]
+  // categories = ["Option 1", "Option 2", "Option 3"]
+  categories: Category[] = []
+  constructor(private categoryService:CategoryService){
+    // We are using reactive programming, so we need to subscribe to the publisher
+    categoryService.getCategories().subscribe(res => {
+      this.categories = res
+    })
+  }
 }
